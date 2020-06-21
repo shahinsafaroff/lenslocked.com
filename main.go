0fe563003f0877bc0df846589b74b52a9ac58992
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-06-21 05:27:56 +0400

    Router and contact page was added

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html") //response writer & set method
-	fmt.Fprint(w,"<h1>Welcome to my awesome WebSite!</h1>")
+	w.Header().Set("Content-Type", "text/html") //response header writer & set method
+	//fmt.Fprint(w,r.URL.Path) //writes and reads the current path in web directory
+	if r.URL.Path =="/" { // this r means router
+		fmt.Fprint(w,"<h1>Welcome to my awesome WebSite!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w,"To get in touch, please send an email to a <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
+
 }
 
 func main () {
