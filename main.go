c47c33edd1436b9ddc54a1a73de3415ffe27f7d3
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-06-21 07:39:55 +0400

    Servemux default router added

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main () {
-	http.HandleFunc("/",handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc) //adds more advanced router pattern / is default route to any subdirectory
+	//http.HandleFunc("/",handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
